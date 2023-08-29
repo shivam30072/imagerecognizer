fdcdbaf3dca38b7b31915718504304c1f23d92bc
Ali Faizan <alifaizan1983@gmail.com>
2023-08-30 00:00:46 +0530

    added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+           <button class="bg-[#1da1f2] text-white ...">
+  Share on Twitter
+</button>
+      <p className='text-3xl'>HELLLO</p>
+ 
+    </div>
+   
+  )
+}
+
+export default App

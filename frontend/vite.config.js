669044df217521fb705badbbcd1b0d52c60aec39
Ali Faizan <alifaizan1983@gmail.com>
2023-09-08 19:46:53 +0530

    changed the url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000", // Don't change this
+      "/api": "https://imageimage.onrender.com/", // Don't change this
     },
   },
 });

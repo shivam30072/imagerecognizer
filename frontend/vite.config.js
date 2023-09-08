1835832975012a6aaf04184f6567605af0be7fb3
Ali Faizan <alifaizan1983@gmail.com>
2023-09-08 19:51:09 +0530

    added another param

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://imageimage.onrender.com/", // Don't change this
+      "/api/image": "https://imageimage.onrender.com/", // Don't change this
     },
   },
 });
